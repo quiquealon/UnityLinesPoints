fd81bb946a2a173e2c21111ff4b7df1c372dc4b3
quiquealon <quiquealonwow@gmail.com>
2020-09-05 10:22:20 -0500

    Upload

---
+++ Assets/LineScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+   
+    private LineRenderer line;
+    private Vector3 mousePos;
+    public Material material;
+    private int currLines = 0;
+    public int numberLines;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            randomLines(numberLines);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            destroyLines();
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (line == null)
+            {
+                createLine();
+            }
+
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
+            line.SetPosition(0, mousePos);
+            line.SetPosition(1, mousePos);
+        }
+
+        else if (Input.GetMouseButtonUp(0) && line)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            line.SetPosition(1, mousePos);
+            line = null;
+            currLines++;
+        }
+
+        else if (Input.GetMouseButton(0) && line)
+        {
+
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            line.SetPosition(1, mousePos);
+
+        }
+    }
+    
+
+    void createLine()
+    {   
+        
+        line = new GameObject("Line" + currLines).AddComponent<LineRenderer>();
+        line.tag = "linea";
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.startColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        line.endColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        line.positionCount = 2;
+        line.startWidth = 8.0f;
+        line.endWidth = 8.0f;
+        line.useWorldSpace = true;
+
+
+
+    }
+
+
+    void destroyLines()
+    {
+        GameObject[] allPoints = GameObject.FindGameObjectsWithTag("linea");
+
+        foreach (GameObject p in allPoints)
+        {
+            Destroy(p);
+        }
+    }
+
+
+    void randomLines(int num)
+    {
+        for (int i=0;i<num;i++)
+        {   
+            Vector3 randomCenter= new Vector3(Random.Range(-900.0F, 900.0F), Random.Range(-500.0F, 500.0F), 0);
+            
+
+            Vector3 position1 =  new Vector3(Random.Range(-35.0F, 35.0F), Random.Range(-35.0F, 35.0F), 0);
+            Vector3 position2 =  new Vector3(Random.Range(-35.0F, 35.0F), Random.Range(-35.0F, 35.0F), 0);
+
+            createLine();
+
+            line.SetPosition(0, randomCenter + position1);
+            line.SetPosition(1, randomCenter + position2);
+            
+            currLines++;
+        }
+       
+    }
+}
---
+++ Assets/PointScrip.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointScrip : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject linePointPrefab;
+
+    public int numberPoints;
+    private int currPoints = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            
+            randomPoints(numberPoints);
+
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            ClearAllPoints();
+        }
+    }
+
+    private void CreatePointMarker(Vector3 pointPosition)
+    {   
+        Instantiate(linePointPrefab, pointPosition, Quaternion.identity);
+    }
+
+    private void ClearAllPoints()
+    {
+        GameObject[] allPoints = GameObject.FindGameObjectsWithTag("PointMarker");
+
+        foreach (GameObject p in allPoints)
+        {
+            Destroy(p);
+        }
+      
+
+    }
+
+    private void randomPoints(int num)
+    {
+        for(int i = 0; i < num; i++)
+        {
+            Vector3 newPos = new Vector3(Random.Range(-940.0F, 940.0F), Random.Range(-520.0F, 520.0F), 0);
+            CreatePointMarker(newPos);
+            currPoints++;
+        }
+    }
+}
